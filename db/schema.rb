c6b06bfe34f6693ef0941dd2c8a13b793f607499
Kyle Langley and Krystan Menne <altck8@gmail.com>
2016-03-23 10:12:15 -0700

    add image model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323170155) do
+ActiveRecord::Schema.define(version: 20160323171029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "user_profile_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "images", ["user_profile_id"], name: "index_images_on_user_profile_id", using: :btree
 
   create_table "user_profiles", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 20160323170155) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "images", "user_profiles"
 end
--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe UserProfile, type: :model do
   it { should belong_to :user }
+  it { should have_many :images }
 end
