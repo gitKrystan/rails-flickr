9c5015a95e059075667d170c742a9dffd21e1beb
Kyle Langley and Krystan Menne <altck8@gmail.com>
2016-03-23 10:09:16 -0700

    update devise html and add user profile model

---
+++ db/migrate/20160323170155_create_user_profiles.rb
@@ -0,0 +1,8 @@
+class CreateUserProfiles < ActiveRecord::Migration
+  def change
+    create_table :user_profiles do |t|
+      t.belongs_to :user, index: true
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323153605) do
+ActiveRecord::Schema.define(version: 20160323170155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
