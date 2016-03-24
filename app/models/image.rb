cbe10332a8652419ac51d9626a8056d1130a3510
Kyle Langley and Krystan Menne <kmenne@gmail.com>
2016-03-24 14:04:47 -0700

    delete comments when an image is deleted

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :profile
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_attached_file :file, styles: { medium: '300x300', thumbnail: '100x100' }
   validates_attachment :file, presence: true,
     content_type: { content_type: /\Aimage\/.*\Z/ }
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Image, type: :model do
   it { should belong_to :profile }
-  it { should have_many :comments }
+  it { should have_many(:comments).dependent(:destroy) }
   it { should have_attached_file :file }
   it { should validate_attachment_content_type(:file)
               .allowing('image/png', 'image/gif')
